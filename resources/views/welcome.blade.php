23cf7aca204f2f5e3724e82eedc9515155b1af0b
Shane Richards <shane.richards121@gmail.com>
2018-03-12 04:12:57 -0500

    changed welcome text to app name, updated proxy settings for heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    MVC Event Planner
                 </div>
 
                 <div class="links">
